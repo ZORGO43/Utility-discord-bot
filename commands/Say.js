cf8ff25e2754c0a0c25a246a8349f9df7e2118e3
Ajmal Rasheed <80527442+ZORGO43@users.noreply.github.com>
2024-03-31 21:55:46 +0530

    Update Say.js

--- commands/Say.js
+++ commands/Say.js
@@ -10,8 +10,8 @@ module.exports = {
 
     async execute(interaction, client) {
         if (    
-            interaction.member.roles.cache.has("1047180959310684280") ||
-            interaction.member.roles.cache.has("839178876298592306")
+            interaction.member.roles.cache.has("AUTH_ROLE_ID") ||
+            interaction.member.roles.cache.has("AUTH_ROLE_ID")
         ) {
             let channel = interaction.options.getChannel('channel');
             let logch = await client.channels.fetch("1153706450313556081");
